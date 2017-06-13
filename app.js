3db229f140f97785e68cc7d7365bf81b8963f085
leon <liyang6956@outlook.com>
2017-06-13 22:17:53 +0800

    count chat num by chatCodeAt

--- app.js
+++ app.js
@@ -8,7 +8,26 @@
         $scope.showMyAge=function () {
           return 18;
         }
+
+        $scope.chartString="";
+        $scope.totalValue=0;
+
+        $scope.displayNumberic=function () {
+
+            var  totalStringValue= calculateNumericForString($scope.chartString);
+
+            $scope.totalValue= totalStringValue;
+        }
+
+            function calculateNumericForString(str) {
+              var totalStringValue=0;
+              for (var i = 0; i < str.length; i++) {
+                totalStringValue+= str.charCodeAt(i);
+              }
+                return totalStringValue;
+            }
     });
+
   }
 
 )();
